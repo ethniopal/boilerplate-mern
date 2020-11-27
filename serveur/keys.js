181a41761b5110f4a0b067feb840a540c908cc6c
ethniopal <olivier.lapointe87@gmail.com>
2020-11-26 22:39:55 -0500

    Delete keys.js

--- serveur/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	MONGOURI:
-		'mongodb+srv://olivier:1Cf2zEd7fed8CqRA@cluster0.uvhkb.mongodb.net/instagramDB?retryWrites=true&w=majority',
-	JWT_SECRET: 'secrettoken',
-	SENDGRID_KEY: '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'
-}
